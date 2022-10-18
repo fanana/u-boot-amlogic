8e4d10ce396397397586b5a330a99ec2ebb6a9b2
Safae Ouajih <souajih@baylibre.com>
2022-10-18 17:07:51 +0200

    android: boot: update android_image_get_data to support v3,v4
    Since boot image header version 3 and 4 introduced vendor boot image,
    the following functions are used to fill the generic android
    structure : andr_image_data:
    
     - android_boot_image_v3_v4_get_data()
     - android_vendor_boot_image_v3_v4_get_data()
    
    android_image_get_data() is then updated to support v3 and v4
    
    Signed-off-by: Safae Ouajih <souajih@baylibre.com>

--- include/image.h
+++ include/image.h
@@ -1627,6 +1627,7 @@ ulong android_image_get_kcomp(const struct andr_boot_img_hdr_v0_v1_v2 *hdr,
 void android_print_contents(const struct andr_boot_img_hdr_v0_v1_v2 *hdr);
 bool android_image_print_dtb_contents(ulong hdr_addr);
 bool is_android_boot_image_header(const struct andr_boot_img_hdr_v0_v1_v2 *boot_img);
+bool is_android_vendor_boot_image_header(const void *vendor_boot_img);
 
 /**
  * board_fit_config_name_match() - Check for a matching board name
